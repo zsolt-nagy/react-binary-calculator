2655404f4aceedb8425070812e053da555354844
Zsolt Nagy <info@zsoltnagy.eu>
2023-07-11 01:21:14 +0200

    calculator buttons

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
     const [current, setCurrent] = useState("0");
     const [currentSign, setCurrentSign] = useState("+");
-    const [isCurrentUpdated, setCurrentIsUpdated] = useState(false);
+    const [isCurrentUpdated, setCurrentIsUpdated] = useState(true);
 
     const [memorySign, setMemorySign] = useState("+");
     const [operator, setOperator] = useState("");
@@ -25,23 +25,63 @@ function App() {
         },
     ]);
 
-    function calculateResult() {
-        alert("TODO calculate result and place it into mem and current");
+    function calculateResult(op = operator) {
+        let leftOperand = Number.parseInt(memory, 2);
+        if (memorySign === "-") {
+            leftOperand *= -1;
+        }
+        let rightOperand = Number.parseInt(current, 2);
+        if (currentSign === "-") {
+            rightOperand *= -1;
+        }
+        let result = null;
+        let resultSign = "+";
+
+        if (op === "+") {
+            result = leftOperand + rightOperand;
+        } else if (op === "-") {
+            result = leftOperand - rightOperand;
+        } else if (op === "*") {
+            result = leftOperand * rightOperand;
+        } else {
+            return;
+        }
+
+        if (result < 0) {
+            resultSign = "-";
+            result *= -1;
+        }
+
+        debugger;
+
+        result = Number(result).toString(2);
+
+        setCurrent(result);
+        setMemory(result);
+        setCurrentSign(resultSign);
+        setMemorySign(resultSign);
+        setCurrentIsUpdated(false);
     }
 
     function executeCommand(op) {
         if (op === "0") {
-            if (current !== "0" && current.length < MAX_LENGTH) {
+            if (!isCurrentUpdated) {
+                setCurrent("0");
+                setCurrentSign("+");
+                setCurrentIsUpdated(true);
+            } else if (current !== "0" && current.length < MAX_LENGTH) {
                 setCurrent(current + "0");
             }
-            setCurrentIsUpdated(true);
         } else if (op === "1") {
-            if (current === "0") {
+            if (!isCurrentUpdated) {
+                setCurrent("1");
+                setCurrentSign("+");
+                setCurrentIsUpdated(true);
+            } else if (current === "0") {
                 setCurrent("1");
             } else if (current.length < MAX_LENGTH) {
                 setCurrent(current + "1");
             }
-            setCurrentIsUpdated(true);
         } else if (op === "+/-") {
             setCurrentSign(currentSign === "+" ? "-" : "+");
         } else if (op === "C") {
@@ -52,10 +92,21 @@ function App() {
             setOperator("");
         } else if (op === "+" || op === "-" || op === "*") {
             setOperator(op);
+
             if (isCurrentUpdated) {
-                calculateResult(); // TODO
-                setCurrentIsUpdated(false);
+                calculateResult(op);
             }
+        } else if (op === "H+") {
+            setHistory((oldHistory) => {
+                let newItem = {
+                    time: new Date().getTime(),
+                    value: currentSign === "-" ? "-" + current : current,
+                };
+                let newHistory = [...oldHistory, newItem];
+                return newHistory;
+            });
+        } else if (op === "=" && operator.length > 0) {
+            calculateResult();
         }
     }
 
